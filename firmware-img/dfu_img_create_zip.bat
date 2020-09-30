e4e72601b91208e2e7712fb0b56d11eb19e3c8d3
DuyLionTran <lowe.tran@gmail.com>
2020-09-30 10:40:46 +0700

    Update bootloader generate script

--- firmware-img/dfu_img_create_zip.bat
+++ firmware-img/dfu_img_create_zip.bat
@@ -1,6 +1,7 @@
 @echo off
 
-%SET APP_VERSION=1
+set /p app_version="Enter application version: "
+set /p hex_file=".hex file name: "
 
 nrfutil keys display --key pk --format code private.pem
-nrfutil pkg generate --hw-version 52 --sd-req 0xAE --application-version 1 --application ..\Project-nRF52840\Output\Debug\Exe\BLE-nRF52840.hex --key-file private.pem ble_cli_dfu_1.zip
+nrfutil pkg generate --hw-version 52 --sd-req 0xCA --application-version %app_version% --application %hex_file% --key-file private.pem ble_%app_version%.zip
